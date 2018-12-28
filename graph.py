9c3d2a252a29aa3e3f5660ae84bea2bd3a47b38c
Caleb J Caldwell <ccaldw2@uwo.ca>
2018-12-28 14:41:41 -0500

    rewrote graph file to describe a wrapper class to handle all my plotting needs

--- graph.py
+++ graph.py
@@ -1,10 +1,21 @@
 import matplotlib.pyplot as plt
 
 
-def graph_cluster(data, labels, means):
-    xs = [n[0] for n in data]
-    ys = [n[1] for n in data]
+class Plot:
+    def __init__(self):
+        self.plot = plt
 
-    plt.scatter(xs, ys, c=labels, s=25, cmap='tab20')
-    plt.scatter(means[:, 0], means[:, 1], c='black', alpha=0.5, s=200)
-    plt.show()
+    def graph_cluster(self, data, labels, means):
+        xs = [n[0] for n in data]
+        ys = [n[1] for n in data]
+
+        self.plot.scatter(xs, ys, c=labels, s=25, cmap='tab20')
+        plt.scatter(means[:, 0], means[:, 1], c='black', alpha=0.5, s=200)
+
+    def graph_ps(self, data):
+        xs = [n[0] for n in data]
+        ys = [n[1] for n in data]
+        self.plot.scatter(xs, ys, c='white', alpha=0.5, s=200)
+
+    def display(self):
+        self.plot.show()
--- main.py
+++ main.py
@@ -9,6 +9,16 @@ data = [[float(n)
         for item in data]
 
 
+tps_locations = [[-79.460830, 43.671080], [-79.486880, 43.694580], [-79.436680, 43.698330],
+                 [-79.425980, 43.651300], [-79.362140, 43.651950], [-79.389720, 43.654210],
+                 [-79.400660, 43.706060], [-79.309480, 43.714610], [-79.317300, 43.669180]]
+
+
 tps_divisions = 17
 clusters, fit = kmca.k_m_clustering(tps_divisions, data)
-gr.graph_cluster(data, fit, clusters)
+
+graph = gr.Plot()
+
+graph.graph_cluster(data, fit, clusters)
+graph.graph_ps(tps_locations)
+graph.display()
